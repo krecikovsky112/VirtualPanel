f7275cc674f7105a6c9dfb78de2b7e0bf26ef2cc
krecikovsky112 <krecikovsky112@gmail.com>
2020-12-08 20:58:33 +0100

    add gas and speedometer and tachometer

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import eu.hansolo.medusa.Gauge;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Gauge Speedometer;
+
+    @FXML
+    private Gauge tachometer;
+
+    @FXML
+    private Button Gas;
+
+    @FXML
+    void handleButtonAction(ActionEvent event) {
+        Speedometer.setAnimated(true);
+        Speedometer.setValue(30);
+        tachometer.setAnimated(true);
+        tachometer.setValue(5);
+    }
+
 }
