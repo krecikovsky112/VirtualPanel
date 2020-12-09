e00025a973756e71acc38cdafdad50596ed3892b
matim1324 <matim1324@gmail.com>
2020-12-09 21:22:09 +0100

    Control methods to load/navigate between two pages 1/2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,13 @@ package sample;
 import eu.hansolo.medusa.Gauge;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class Controller {
 
@@ -17,6 +23,12 @@ public class Controller {
     private Button Gas;
 
     @FXML
+    private BorderPane comp_bg;
+
+    @FXML
+    private AnchorPane comp_display;
+
+    @FXML
     void handleButtonAction(ActionEvent event) {
         Speedometer.setAnimated(true);
         Speedometer.setValue(30);
@@ -24,4 +36,20 @@ public class Controller {
         tachometer.setValue(5);
     }
 
+    @FXML
+    void right_navigation(ActionEvent event) throws IOException {
+        loadPage("page1");
+    }
+
+    @FXML
+    void left_navigation(ActionEvent event) throws IOException {
+        loadPage("page2");
+    }
+
+    private void loadPage(String page) throws IOException {
+        Parent root = null;
+
+        root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+        comp_bg.setCenter(root);
+    }
 }
