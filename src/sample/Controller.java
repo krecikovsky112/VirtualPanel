3fe40cd5a6463839720185d744c9c04e07b6622b
krecikovsky112 <krecikovsky112@gmail.com>
2020-12-13 20:28:22 +0100

    add testing brake button and change action on speeedometer and tachometer

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,13 @@ package sample;
 import eu.hansolo.medusa.Gauge;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class Controller {
 
@@ -17,11 +23,57 @@ public class Controller {
     private Button Gas;
 
     @FXML
-    void handleButtonAction(ActionEvent event) {
+    private BorderPane comp_bg;
+
+    @FXML
+    private AnchorPane comp_display;
+
+    int speedValue=0;
+    int tachoValue=0;
+    @FXML
+    void onPressed(ActionEvent event) throws InterruptedException {
         Speedometer.setAnimated(true);
-        Speedometer.setValue(30);
+        speedValue+=15;
+        if(speedValue>=200)
+        {
+            speedValue=195;
+        }
+        Speedometer.setValue(speedValue);
         tachometer.setAnimated(true);
-        tachometer.setValue(5);
+        if(tachoValue==8)
+        {
+            tachoValue=0;
+        }
+            tachoValue += 4;
+        if(speedValue==195)
+        {
+            tachoValue=8;
+        }
+        tachometer.setValue(tachoValue);
     }
 
+    @FXML
+    void onRelease(ActionEvent event)
+    {
+        Speedometer.setAnimated(true);
+        speedValue-=25;
+        Speedometer.setValue(speedValue);
+    }
+
+    @FXML
+    void right_navigation(ActionEvent event) throws IOException {
+        loadPage("page1");
+    }
+
+    @FXML
+    void left_navigation(ActionEvent event) throws IOException {
+        loadPage("page2");
+    }
+
+    private void loadPage(String page) throws IOException {
+        Parent root = null;
+
+        root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+        comp_bg.setCenter(root);
+    }
 }
